a41863c89a609ca1ddfa1e90b3beab8b6de7f316
Ryan Daniel Pangaribuan <ryan.pangaribuan@seamoney.com>
2023-03-05 11:05:36 +0000

    Feat/sl 454 admin reply

--- src/components/molecules/Sidebar/index.tsx
+++ src/components/molecules/Sidebar/index.tsx
@@ -1,42 +1,60 @@
-import { UserOutlined } from '@ant-design/icons';
+import { HomeOutlined, TagOutlined, TagsOutlined } from '@ant-design/icons';
 import React from 'react';
 import { Menu } from '../..';
 import style from './index.module.scss';
 import { Logo, LogoIcon } from '../../atoms';
-import { Layout } from 'antd';
-import { Link, useLocation } from 'react-router-dom';
+import { Layout, MenuProps } from 'antd';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { MenuDividerType } from 'rc-menu/lib/interface';
 
 interface SidebarProps {
   collapsed: boolean;
 }
 
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: 'group',
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem;
+}
+
 const items = [
-  {
-    key: '',
-    icon: <UserOutlined />,
-    label: <Link to="/">Home</Link>,
-  },
-  {
-    key: 'promotions',
-    icon: <UserOutlined />,
-    label: <Link to="/promotions">Promotion</Link>,
-  },
-  {
-    key: 'vouchers',
-    icon: <UserOutlined />,
-    label: <Link to="/vouchers">Voucher</Link>,
-  },
-  {
-    key: 'refund',
-    icon: <UserOutlined />,
-    label: <Link to="/refunds">Refund</Link>,
-  },
+  getItem('Home', '', <HomeOutlined />),
+  { type: 'divider' } as MenuDividerType,
+  getItem('Promotions', 'sub1', <TagOutlined />, [
+    getItem('Promotion List', 'promotions'),
+    getItem('Create Promotion Banner', 'promotions/create-banner'),
+  ]),
+  getItem('Vouchers', 'sub2', <TagsOutlined />, [
+    getItem('Voucher List', 'vouchers'),
+    getItem('Create Voucher', 'vouchers/create'),
+  ]),
 ];
+
 const { Sider } = Layout;
 
+const defaultOpenKeys = ['sub1', 'sub2'];
+
 const Sidebar: React.FC<SidebarProps> = ({ collapsed }) => {
+  const navigate = useNavigate();
   const location = useLocation();
-  const lastPath = location.pathname.split('/').pop();
+
+  const lastPath = location.pathname.split('/').slice(2).join('/');
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    navigate(`/${e.key}`);
+  };
 
   return (
     <Sider
@@ -53,8 +71,11 @@ const Sidebar: React.FC<SidebarProps> = ({ collapsed }) => {
       )}
       <Menu
         className={style.sidebar__menu}
-        defaultSelectedKeys={lastPath ? [lastPath] : ['']}
         items={items}
+        mode="inline"
+        defaultOpenKeys={defaultOpenKeys}
+        defaultSelectedKeys={[lastPath || '']}
+        onClick={onClick}
       />
     </Sider>
   );
