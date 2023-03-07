c15433485f518c33383d8e0486336ae0a3d0c839
Ryan Daniel Pangaribuan <ryan.pangaribuan@seamoney.com>
2023-03-07 13:46:14 +0000

    Feat/helmet

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { router } from './helpers/route/routes';
 import { RouterProvider } from 'react-router-dom';
 import theme from './helpers/theme';
 import { HelmetProvider } from 'react-helmet-async';
+import { SEO } from './components';
 
 function App(): JSX.Element {
   return (
     <HelmetProvider>
       <ConfigProvider theme={theme}>
+        <SEO title="Blanche Admin" description="Blanche Admin page" />
         <Suspense
           fallback={
             <div style={{ margin: '10rem' }}>
